77d9d192b8ccb825a54ca4997c2f6357486f188e
yossu-y <yangyuub@gmail.com>
2022-04-09 10:10:45 +0000

    [update]

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -34,7 +34,7 @@ class ListsController < ApplicationController
   
   def destroy
     list = List.find(params[:id])
-    list.destroyredirect_to "/lists"
+    list.destroy redirect_to "/lists"
   end
     
   private
