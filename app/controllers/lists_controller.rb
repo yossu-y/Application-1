9f7867f62d3768baff53abd1776fabd84cdfc94a
yossu-y <yangyuub@gmail.com>
2022-05-28 01:23:54 +0000

    update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,20 +1,22 @@
 class ListsController < ApplicationController
-  
+
   def new
     @list = List.new
   end
-  
+
   def create
     @list = List.new(list_params)
     if @list.save
       redirect_to list_path(@list.id)
     else
       render :new
-    end  
+    end
   end
 
 
   def index
+    puts "作成したキー #{ENV['SECRET_KEY']}"
+
     @lists = List.all
   end
 
@@ -25,21 +27,22 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
-    list.destroy redirect_to "/lists"
+    list.destroy
+    redirect_to "/lists"
   end
-    
+
   private
   def list_params
     params.require(:list).permit(:title,:body,:image)
   end
-  
+
 end
